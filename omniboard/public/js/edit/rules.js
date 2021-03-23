fb13d0090d204833cba37c3a1323305ee6b3d323
Baptiste <baptiste.meauze.35@gmail.com>
2021-03-23 16:18:47 +0100

    c'est tmp tkt

--- omniboard/public/js/edit/rules.js
+++ omniboard/public/js/edit/rules.js
@@ -141,7 +141,7 @@ function addListener(){
     var acc = document.getElementsByClassName("accordion");
     var i;
 
-    for (i = 0; i < acc.length; i++) {;
+    for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
             var panel = this.nextElementSibling;
@@ -183,3 +183,8 @@ function addRuleListener() {
             } 
         });
     }
+}
+
+function getRulesFromFile(){
+    console.log("test")
+}
