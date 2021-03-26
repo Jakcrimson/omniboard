ec0da3b8baf144ee4583760e139478282e4b5cd8
Benjamin-Viaud <viaud.e1702527@etud.univ-ubs.fr>
2021-03-26 15:18:01 +0100

    delete rule + delete action and condition

--- omniboard/public/js/edit/rules.js
+++ omniboard/public/js/edit/rules.js
@@ -1,9 +1,9 @@
 var numberC = 0; //number of condition block
 var numberA = 0; //number of action block
 var numberR = 0; //number of rule
-var listCond = []; //list the number of condition in the condition block with the index is the
+var listCond = new Array(new Array()); //list the number of condition in the condition block with the index is the
 //num of the condition block
-var listAct = []; //list the number of action in the action block with the index is the
+var listAct = [[]]; //list the number of action in the action block with the index is the
 //num of the action block
 var blockList = {
 
@@ -79,9 +79,11 @@ function getInput(x) {
 }
 
 function addRule() {
+    console.log(listCond);
     d = document.getElementById('rule');
     d.innerHTML += "<button class='accordionR' id='accordionR" + numberR + "' onclick='addRuleListener("+numberR+")'>" + 'Rule' + numberR + "</button>" +
         "<div class='panel1' id='Rule" + numberR + "'>" +
+        "<button for='Delete' onClick='deleteRule(" + numberR +")'> delete Rule</button><br>" +
         "<label for='type'> type :<br />" +
         "<input id='type" + numberR + "' type='text' name='search' placeholder='Enter the type of the rule' /><br />" +
         "<label for='name'> name :</label><br />" +
@@ -94,6 +96,13 @@ function addRule() {
     addRuleListener();
 }
 
+function deleteRule(x) {
+    if(document.getElementsByClassName("accordionR" + x) != undefined) {
+        document.getElementById("Rule" + x).remove();
+        document.getElementById("accordionR" + x).remove();
+    }
+}
+
 function getInputFromCond(x, nbRule) {
     var name = document.getElementById(nbRule + 'name' + x)
     var input = document.getElementById(nbRule + 'inputLoop' + x)
@@ -104,15 +113,15 @@ function getInputFromCond(x, nbRule) {
         input = document.getElementById(nbRule + 'inputText' + x)
     }
 
-    var blockList = JSON.parse(window.localStorage.getItem("blockList"))
-    console.log(blockList[nbRule])
+    // var blockList = JSON.parse(window.localStorage.getItem("blockList"))
+    // console.log(blockList)
 
-    blockList[nbRule].conditions[x] = {
-        'name': name.value,
-        'input': input.value,
-        'operation': operation.value,
-        'value': value.value
-    }
+    // blockList[nbRule].conditions[x] = {
+    //     'name': name.value,
+    //     'input': input.value,
+    //     'operation': operation.value,
+    //     'value': value.value
+    // }
     window.localStorage.setItem("blockList", JSON.stringify(blockList));
 }
 
@@ -126,17 +135,18 @@ function updateInput(x, nbRule) {
 
 
 function addCondition(x) {
-    if (listCond[x] != undefined) {
-        listCond[x] += 1;
+    listCond[x] = new Array()
+    if (listCond[x][numberC] != undefined) {
+        listCond[x][numberC] += 1;
     } else {
-        listCond[x] = 1;
+        listCond[x][numberC] = 1;
     }
     d = document.getElementById('Rule' + x);
     var l = document.createElement("conditions" + x);
-    l.innerHTML += "<button class='accordion' id='accordionC" + numberC + "" + listCond[x] + "' onclick=getInputFromCond(" + numberC + "," + x + ");addListener()>" + 'Condition' + numberC + "</button>" +
-        "<div class='panel' id='Condition" + numberC + "'>" +
-        "<button for='name' onClick='del(" + x + ", " + listCond[x]+")'> delete </button><br>" +
-        "<div class='con" + numberC + "'>" +
+    l.innerHTML += "<button class='accordion' id='accordionC" + x + "" + 1 + "' onclick=getInputFromCond(" + numberC + "," + x + ");addListener()>" + 'Condition' + x + "." + (numberC+1) + "</button>" +
+        "<div class='panel' id='Condition" + x + "'>" +
+        "<button for='name' onClick='del(" + x + ", " + 1+")'> delete </button><br>" +
+        "<div class='con" + x + "'>" +
         "<label for='name'> name :</label><br>" +
         "<input id='" + x + "name" + numberC + "' type='text' name='search' placeholder='Enter the name of the condition' /><br />" +
         "<label for='action'> input :</label><br>" +
@@ -155,8 +165,8 @@ function addCondition(x) {
         "<option value=greater_than>greater_than</option></select><br>" +
         "<label for='name'> value :</label><br>" +
         "<input type='text' id='" + x + "value" + numberC + "' name='search'/><br>" +
-        "<button for='name' onClick='addConditionElement(" + numberC + ")'> addCondition </button>" +
-        "<button for='name' onClick='delOne(" + numberC + ", " + listCond[x] + ")'> delete Condition </button><br>" +
+        "<button for='name' onClick='addConditionElement(" + x + ")'> addCondition </button>" +
+        "<button for='name' onClick='delOneCond(" + x + ", " + listCond[x][numberC] + ")'> delete Condition </button><br>" +
         "<label for='name'>-------------------------------------------------------</label><br>";
     d.appendChild(l);
     numberC += 1;
@@ -164,8 +174,7 @@ function addCondition(x) {
 }
 
 function addConditionElement(x) {
-    console.log("neter")
-    listCond[x] += 1;
+    listCond[x][numberC-1] += 1;
     d = document.getElementById("Condition" + x);
     var l = document.createElement("conditions" + x);
     l.innerHTML += "<div class='con" + x + "'>" +
@@ -187,24 +196,89 @@ function addConditionElement(x) {
         "<option value=greater_than>greater_than</option></select><br>" +
         "<label for='name'> value :</label><br>" +
         "<input type='text' id='" + x + "value" + numberC + "' name='search'/><br>" +
-        "<button for='name' onClick='addConditionElement(" + numberC + ")'> addCondition </button>" +
-        "<button for='name' onClick='delOne(" + numberC + ", " + listCond[x] + ")'> delete Condition </button><br>" +
+        "<button for='name' onClick='addConditionElement(" + x + ")'> add Condition </button>" +
+        "<button for='name' onClick='delOneCond(" + x + ", " + listCond[x][numberC-1] + ")'> delete Condition </button><br>" +
         "<label for='name'>-------------------------------------------------------</label><br>";
     d.appendChild(l);
 }
 
-function addAction(x) {
-    if (listAct[x] != undefined) {
-        listAct[x] += 1;
+function delOneCond(x, y) {
+    size = document.getElementsByClassName('con' + x).length;
+    console.log(size);
+    if (size == 1) {
+        if (document.getElementById('Condition' + x) != null) {
+            document.getElementById('Condition' + x).remove();
+            document.getElementById('accordionC' + x + "" + 1).remove();
+        }
     } else {
-        listAct[x] = 1;
+        if (document.getElementsByClassName('con' + x)[y-1] != null) {
+            document.getElementsByClassName('con' + x)[y - 1].remove();
+        }
     }
+}
+
+function addAction(x) {
+    listAct[x] = new Array();
+    if (listAct[x][numberA] != undefined) {
+        listAct[x][numberA] += 1;
+    } else {
+        listAct[x][numberA] = 1;
+    }
+    console.log(listAct[x][numberA]);
     d = document.getElementById('Rule' + x);
     var l = document.createElement("conditions" + x);
-    l.innerHTML += "<button class='accordion' id='accordionA" + x + "" + listAct[x] + "' onclick='addListener()'>" + 'Action' + x + "." + listAct[x] + "</button>" +
+    l.innerHTML += "<button class='accordion' id='accordionA" + x + "" + 1 + "' onclick='addListener()'>" + 'Action' + x + "." + (numberA+1) + "</button>" +
         "<div class='panel' id='Action" + x + "'>" +
-        "<button for='name' onClick='del(" + x + ", " + listAct[x]+")'> delete </button><br>" +
+        "<button for='name' onClick='del(" + x + ", " + listAct[x][numberA]+")'> delete </button><br>" +
         "<div class='act" + x + "'>" +
+        "<label for='name'> name :</label><br>" +
+        "<input type='text' id='" + x + "nameText" + numberA + "' name='search'/><br>" +
+        "<label for='action'> action :</label><br>" +
+        "<select id='" + x + "inputLoop" + numberA + "' name='loop1' onclick=updateInput(" + numberA + "," + x + ")>" +
+        "<option value=action_block_name >action_block_name </option>" +
+        "<option value=play >play </option>" +
+        "<option value=flash >flash </option>" +
+        "<option value=increase >increase </option>" +
+        "<option value=decrease >decrease</option>" +
+        "<option value=multiply >multiply </option>" +
+        "<option value=divide >divide </option>" +
+        "<option value=set >set </option>" +
+        "<option value=up >up </option>" +
+        "<option value=down >down </option>" +
+        "<option value=reset >reset </option></select><br>" +
+        "<select disabled=true id='" + x + "inputLoop" + x + "' name='loop1' onclick=updateInput(" + numberA + "," + x + ")>" +
+        "<option value=choose>choose action block </option></select><br>" +
+        "<input type='text' disabled=true id='" + x + "actionText" + numberA + "' name='search'/><br>" +
+        "<label for='output'> output :</label><br>" +
+        "<input type='text' id='" + x + "outputText" + numberA + "' name='search'/><br>" +
+        "<label for='value'> value :</label><br>" +
+        "<select id='" + x + "operationLoop" + numberA + "' name='loop2'>" +
+        "<option value='value'> value </option>" +
+        "<option value=file_name >file_name </option>" +
+        "<option value=true >true </option>" +
+        "<option value=false >false </option>" +
+        "<option value=up >up </option>" +
+        "<option value=down >down </option>" +
+        "<option value=on >on </option>" +
+        "<option value=off >off </option>" +
+        "<option value=variable> variable </option>" +
+        "<option value=formula> formula </option></select><br>" +
+        "<input type='text' id='" + x + "inputText" + numberA + "' name='search'/><br>" +
+        "<label for='param'> param :</label><br>" +
+        "<input type='text' id='" + x + "paramText" + numberA + "' name='search'/><br>" +
+        "<button for='name' onClick='addActionElement(" + x + ")'> addAction </button>" +
+        "<button for='name' onClick='delOneAct(" + x + ", " + listAct[x][numberA] + ")'> delete Action </button><br>" +
+        "<label for='name'>-------------------------------------------------------</label><br>";
+    d.appendChild(l);
+    numberA += 1;
+    addListener();
+}
+
+function addActionElement(x) {
+    listAct[x][numberA] += 1;
+    d = document.getElementById("Action" + x);
+    var l = document.createElement("conditions" + x);
+    l.innerHTML += "<div class='act" + x + "'>" +
         "<label for='name'> name :</label><br>" +
         "<input type='text' id='" + x + "inputText" + numberA + "' name='search'/><br>" +
         "<label for='action'> action :</label><br>" +
@@ -240,40 +314,25 @@ function addAction(x) {
         "<input type='text' id='" + x + "inputText" + numberA + "' name='search'/><br>" +
         "<label for='param'> param :</label><br>" +
         "<input type='text' id='" + x + "inputText" + numberA + "' name='search'/><br>" +
-        "<button for='name' onClick='addActionElement(" + x + ")'> addAction </button>" +
-        "<button for='name' onClick='delOne(" + x + ", " + listAct[x] + ")'> delete Action </button><br>" +
+        "<button for='name' onClick='addActionElement(" + x + ")'> add Action </button>" +
+        "<button for='name' onClick='delOneAct(" + x + ", " + listAct[x][numberA-1] + ")'> delete Action </button><br>" +
         "<label for='name'>-------------------------------------------------------</label><br>";
     d.appendChild(l);
-    numberA += 1;
-    addListener();
 }
 
-function addActionElement(x) {
-    listAct[x] += 1;
-    d = document.getElementById("Action" + x);
-    var l = document.createElement("conditions" + x);
-    l.innerHTML += "<div class='act" + x + "'>" +
-        "<label for='name'> name :</label><br>" +
-        "<select id='name' name='name'>script:element()</select><br>" +
-        "<label for='name'> input :</label><br>" +
-        "<select id='loop1' name='loop1'>" +
-        "<option value=1>input</option>" +
-        "<option value=2 >conditional_block</option>" +
-        "<option value=3 >variable</option>" +
-        "<option value=4>formula</option></select><br>" +
-        "<input type='text' name='search'/><br>" +
-        "<label for='name'> operation :</label><br>" +
-        "<select id='loop2' name='loop2'>" +
-        "<option value=1>less_than</option>" +
-        "<option value=2 >equals </option>" +
-        "<option value=3 >not_equals </option>" +
-        "<option value=4>greater_than</option></select><br>" +
-        "<label for='name'> value :</label><br>" +
-        "<input type='text' name='search'/><br>" +
-        "<button for='name' onClick='addActElement(" + x + ")'> addAct </button>" +
-        "<button for='name' onClick='delOne(" + x + ", " + listAct[x] + ")'> delete Action </button><br>" +
-        "<label for='name'>-------------------------------------------------------</label><br>";
-    d.appendChild(l);
+function delOneAct(x, y) {
+    size = document.getElementsByClassName('act' + x).length;
+    if (size == 1) {
+        if (document.getElementById('Action' + x) != null) {
+            document.getElementById('Action' + x).remove();
+            document.getElementById('accordionA' + x + "" + 1).remove();
+        }
+    } else {
+        console.log(document.getElementsByClassName('act' + x)[y-1]);
+        if (document.getElementsByClassName('act' + x)[y-1] != null) {
+            document.getElementsByClassName('act' + x)[y - 1].remove();
+        }
+    }
 }
 
 function del(x, y) {
@@ -283,25 +342,6 @@ function del(x, y) {
     } else if (document.getElementById('Condition' + x) != null) {
         document.getElementById('Condition' + x).remove();
         document.getElementById('accordionC' + x +"" +y).remove();
-    }
-}
-
-function delOne(x, y) {
-    size = document.getElementsByClassName('con' + x).length;
-    if (size == 1) {
-        if (document.getElementById('Action' + x) != null) {
-            document.getElementById('Action' + x).remove();
-            document.getElementById('accordionA' + x).remove();
-        } else if (document.getElementById('Condition' + x) != null) {
-            document.getElementById('Condition' + x).remove();
-            document.getElementById('accordionC' + x).remove();
-        }
-    } else {
-        if (document.getElementById('Action' + x) != null) {
-            document.getElementById('Action' + x).remove();
-        } else if (document.getElementsByClassName('con' + x)[y] != null) {
-            document.getElementsByClassName('con' + x)[y - 1].remove();
-        }
     }
 }
 
